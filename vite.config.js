14d198e32c51779a0563dfb003a700e7724408fc
David Mite <108368356+davismz09@users.noreply.github.com>
2024-02-25 19:41:57 -0500

    Cambio en la ruta

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://dmitez.github.io/react-rick-morty/",
+  base: "https://davismz09.github.io/react-rick-morty/",
   plugins: [react()],
 });
